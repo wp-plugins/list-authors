b3c515e853fd627fd32f9f9f69cfd7063eb946b6
Takaitra <Takaitra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-05 22:02:24 +0000

    Version 2.0 of the List Authors widget. Introduces a custom wp_list_authors template tag to allow for sorting and limiting the number of authors displayed.
    git-svn-id: https://plugins.svn.wordpress.org/list-authors/trunk@285525 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-list-authors.php
+++ class-list-authors.php
@@ -5,11 +5,11 @@
  * @package ListAuthors
  * @since 1.2
  *
- * @version 1.0
+ * @version 2.0
  * @copyright Matthew Toso
  * @author Matthew Toso
- * @link http://www.takaitra.com/posts/60
- * @license LGPL License http://www.opensource.org/licenses/lgpl-license.html
+ * @link http://www.takaitra.com/projects/list-authors
+ * @license GPL License http://www.opensource.org/licenses/gpl-license.html
  */
 class ListAuthorsWidget extends WP_Widget {
     /** constructor */
@@ -32,7 +32,7 @@ class ListAuthorsWidget extends WP_Widget {
             echo '<ul>';
         ?>
         <!-- List Authors -->
-          <?php wp_list_authors('optioncount='.$options['optioncount'].'&exclude_admin='.$options['exclude_admin'].'&show_fullname='.$options['show_fullname'].'&hide_empty='.$options['hide_empty'].$feed_image.'&style='.$options['style']); ?>
+          <?php wp_list_authors2('optioncount='.$options['optioncount'].'&exclude_admin='.$options['exclude_admin'].'&show_fullname='.$options['show_fullname'].'&hide_empty='.$options['hide_empty'].$feed_image.'&style='.$options['style'].'&orderby='.$options['orderby'].'&order='.$options['order'].'&min_count='.$options['min_count'].'&number='.$options['number']); ?>
         <!-- /List Authors -->
         <?php
         if ( $options['style'] == 'list' )
@@ -47,9 +47,14 @@ class ListAuthorsWidget extends WP_Widget {
         $instance['optioncount'] = isset($new_instance['optioncount']);
         $instance['exclude_admin'] = isset($new_instance['exclude_admin']);
         $instance['show_fullname'] = isset($new_instance['show_fullname']);
-        $instance['hide_empty'] = isset($new_instance['hide_empty']);
+        $instance['hide_empty'] = false;
         $instance['show_feedimage'] = isset($new_instance['show_feedimage']);
         $instance['style'] = $new_instance['style'];
+		$instance['orderby'] = $new_instance['orderby'];
+		$instance['order'] = $instance['orderby'] == 'name' ? 'ASC' : 'DESC';
+		$instance['number'] = intval($new_instance['number']) > 0 ? intval($new_instance['number']) : NULL;
+		$instance['min_count'] = intval($new_instance['min_count']) > 0 ? intval($new_instance['min_count']) : NULL;
+
         return $instance;
     }
 
@@ -62,30 +67,44 @@ class ListAuthorsWidget extends WP_Widget {
         $show_fullname = $options['show_fullname'] ? 'checked="checked"' : '';
         $hide_empty = $options['hide_empty'] ? 'checked="checked"' : '';
         $feed_image = $options['show_feedimage'] ? 'checked="checked"' : '';
-	$list_selected = $options['style'] == 'list' ? 'selected' : '';
-	$none_selected = $options['style'] == 'none' ? 'selected' : '';
+        $list_selected = $options['style'] == 'list' ? 'selected' : '';
+        $none_selected = $options['style'] == 'none' ? 'selected' : '';
+        $sort_by_name_selected = $options['orderby'] == 'name' ? 'selected' : '';
+        $sort_by_count_selected = $options['orderby'] == 'count' ? 'selected' : '';
+		$number = $options['number'];
+		$min_count = $options['min_count'];
 
-	// Warning for Bug #10328
+        // Warning for Bug #10328
         if ( version_compare( $GLOBALS['wp_version'], '2.8.3', '<' ) ) {
             if ( $options['hide_empty'] && $options['style'] == 'none' ) {
                 echo '<p><strong>Warning:</strong> Due to a bug in WordPress 2.8, there is no output when "Hide authors with 0 posts" is selected along with the comma-separated style.</p>';
             }
-	}
+        }
 
         ?>
-            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
 	    <p>
-		<label for="<?php echo $this->get_field_id('style'); ?>" class="screen-reader-text"><?php _e('Display Style:'); ?></label>
-		<select class="widefat" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>">
-                    <option value="list" <?php echo $list_selected; ?>>List Style</option>
-		    <option value="none" <?php echo $none_selected; ?>>Comma-Separated Style</option>
-                </select>
-            </p>
-            <p><label for="<?php echo $this->get_field_id('optioncount'); ?>"><input class="checkbox" type="checkbox" <?php echo $optioncount; ?> id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('optioncount'); ?>" /> <?php _e('Show number of published posts'); ?></label><br />
+            <label for="<?php echo $this->get_field_id('style'); ?>" class="screen-reader-text"><?php _e('Display Style:'); ?></label>
+            <select class="widefat" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>">
+                <option value="list" <?php echo $list_selected; ?>>List Style</option>
+		        <option value="none" <?php echo $none_selected; ?>>Comma-Separated Style</option>
+            </select>
+        </p>
+	    <p>
+            <label for="<?php echo $this->get_field_id('orderby'); ?>" class="screen-reader-text"><?php _e('Sort:'); ?></label>
+            <select class="widefat" id="<?php echo $this->get_field_id('orderby'); ?>" name="<?php echo $this->get_field_name('orderby'); ?>">
+                <option value="name" <?php echo $sort_by_name_selected; ?>>Sort by Name</option>
+		        <option value="count" <?php echo $sort_by_count_selected; ?>>Sort by Post Count</option>
+            </select>
+        </p>
+        <p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Maximum displayed:'); ?></label> <input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" size="3" value="<?php echo $number; ?>" /></p>
+        <p><label for="<?php echo $this->get_field_id('min_count'); ?>"><?php _e('Minimum author posts:'); ?></label> <input id="<?php echo $this->get_field_id('min_count'); ?>" name="<?php echo $this->get_field_name('min_count'); ?>" type="text" size="3" value="<?php echo $min_count; ?>" /></p>
+		<p>
+            <label for="<?php echo $this->get_field_id('optioncount'); ?>"><input class="checkbox" type="checkbox" <?php echo $optioncount; ?> id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('optioncount'); ?>" /> <?php _e('Show number of published posts'); ?></label><br />
             <label for="<?php echo $this->get_field_id('exclude_admin'); ?>"><input class="checkbox" type="checkbox" <?php echo $exclude_admin; ?> id="<?php echo $this->get_field_id('exclude_admin'); ?>" name="<?php echo $this->get_field_name('exclude_admin'); ?>" /> <?php _e('Exclude administrator'); ?></label><br />
             <label for="<?php echo $this->get_field_id('show_fullname'); ?>"><input class="checkbox" type="checkbox" <?php echo $show_fullname; ?> id="<?php echo $this->get_field_id('show_fullname'); ?>" name="<?php echo $this->get_field_name('show_fullname'); ?>" /> <?php _e('Show full name'); ?></label><br />
-            <label for="<?php echo $this->get_field_id('hide_empty'); ?>"><input class="checkbox" type="checkbox" <?php echo $hide_empty; ?> id="<?php echo $this->get_field_id('hide_empty'); ?>" name="<?php echo $this->get_field_name('hide_empty'); ?>" /> <?php _e('Hide authors with 0 posts'); ?></label><br />
-            <label for="<?php echo $this->get_field_id('show_feedimage'); ?>"><input class="checkbox" type="checkbox" <?php echo $feed_image; ?> id="<?php echo $this->get_field_id('show_feedimage'); ?>" name="<?php echo $this->get_field_name('show_feedimage'); ?>" /> <?php _e('Show an RSS feed image and link'); ?></label></p>
+			<label for="<?php echo $this->get_field_id('show_feedimage'); ?>"><input class="checkbox" type="checkbox" <?php echo $feed_image; ?> id="<?php echo $this->get_field_id('show_feedimage'); ?>" name="<?php echo $this->get_field_name('show_feedimage'); ?>" /> <?php _e('Show an RSS feed image and link'); ?></label>
+        </p>
         <?php 
     }
 
@@ -98,7 +117,11 @@ class ListAuthorsWidget extends WP_Widget {
             'show_fullname' => false,
             'hide_empty' => false,
             'show_feedimage' => true,
-            'style' => 'list'
+			'style' => 'list',
+			'orderby' => 'name',
+			'order' => 'ASC',
+			'number' => 10,
+			'min_count' => NULL
         );
     }
 
--- readme.txt
+++ readme.txt
@@ -2,21 +2,22 @@
 Contributors: Takaitra
 Tags: authors, widget, sidebar
 Requires at least: 2.0.2
-Tested up to: 2.9
+Tested up to: 3.0.1
 Stable tag: trunk
 
 A widget to display a list of site authors.
 
 == Description ==
 
-A widget to display a list of authors in your WordPress blog. Includes options to enable/disable the features mentioned below. Version 1.2 is completely updated to use the new widget API and is multi-widget enabled. Fully XHTML compliant.
+A widget to display a list of authors in your WordPress blog. Includes widget options to configure the features mentioned below. Completely updated to use the new widget API and is multi-widget enabled. Fully XHTML compliant.
 
 #### Features:
 * Choose between an HTML list or comma-separated list.
 * Can show number of published author posts.
+* Sort authors by name or by post count.
+* Exclude users below a given post count threshold.
 * Allows administrator to be excluded from the list.
 * Choose between displaying usernames or full names in the list.
-* Allows users with 0 posts to be excluded from the list.
 * Can include links to author-specific RSS feeds.
 
 
@@ -30,37 +31,9 @@ A widget to display a list of authors in your WordPress blog. Includes options t
 
 == Frequently Asked Questions ==
 
-= Can I sort the list of authors alphabetically or by post count? =
-
-No. This widget depends on the standard template tag [wp_list_authors](http://codex.wordpress.org/Template_Tags/wp_list_authors) which does not provide this ability. I have created a [feature request](http://core.trac.wordpress.org/ticket/10329) and, if I have time, I'll create and submit a patch.
-
-= Can I limit the number of authors listed in the widget? =
-
-This is a common request from those who have many contributors to their WordPress site (sometimes hundreds). Unfortunately, the answer for now is the same as above.
-
-= What if I have further questions? =
-
-If you have any questions or comments, feel free to [leave a comment](http://www.takaitra.com/projects/list-authors) on the project page and I will respond as soon as I can.
+If you have any questions or comments, feel free to [contact me](http://www.takaitra.com).
 
 == Screenshots ==
 
 1. The List Authors widget showing two authors along with links to their respective RSS feeds.
 2. The List Authors widget configuration.
-
-== Changelog ==
-
-= 1.2 =
-* Updated to use the new widget API introduced in WordPress 2.8.
-* Added the HTML/comma-separated list dropdown.
-* Multi-widget enabled (it is now possible to have two or more copies of the same widget on one blog).
-
-= 1.1.1 =
-* Small fix to make the widget fully XHTML compliant.
-
-= 1.1 =
-* Initial release.
-
-== Upgrade Notice ==
-
-= 1.2 =
-Major update to use the new widget API and enable multi-widget functionality.
